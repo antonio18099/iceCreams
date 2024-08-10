2fb9446efcfc138f842c58cd4620f8ea6729745b
antonio18099 <antoniosanchezr0921@gmial.com>
2024-08-10 09:24:36 -0500

    segundo commit se hiceron cambios en el js y en el css

--- scripts/products.js
+++ scripts/products.js
@@ -1,62 +1,62 @@
 export const iceCreams = [
-  {
-    product: "Chocolate Supremo",
+    {
+        product: "Chocolate Supremo",
     id: 1,
     price: 3.50,
     image: "assets/images/chocolate.jpg"
-  },
-  {
+    },
+    {
     product: "Vainilla Clásica",
     id: 2,
     price: 3.00,
     image: "assets/images/Vainilla.jpg"
-  },
-  {
+    },
+    {
     product: "Fresa Deliciosa",
     id: 3,
     price: 3.25,
     image: "assets/images/fresa.jpg"
-  },
-  {
+    },
+    {
     product: "Mango Tropical",
     id: 4,
     price: 3.75,
     image: "assets/images/Mango.jpg"
-  },
-  {
+    },
+    {
     product: "Cookies & Cream",
     id: 5,
     price: 4.00,
     image: "assets/images/galletas.jpg"
-  },
-  {
+    },
+    {
     product: "Pistacho Gourmet",
     id: 6,
     price: 3.50,
     image: "assets/images/Pistacho.jpg"
-  },
-  {
+    },
+    {
     product: "Caramelo Salado",
     id: 7,
     price: 3.60,
     image: "assets/images/Caramelo.jpg"
-  },
-  {
+    },
+    {
     product: "Café Expreso",
     id: 8,
     price: 3.80,
     image: "assets/images/Cafe.jpg"
-  },
-  {
+    },
+    {
     product: "Menta con Chocolate",
     id: 9,
     price: 3.40,
     image: "assets/images/Menta.jpg"
-  },
-  {
+    },
+    {
     product: "Frambuesa Silvestre",
     id: 10,
     price: 3.70,
     image: "assets/images/Frambuesa.jpg"
-  }
+    }
 ]
