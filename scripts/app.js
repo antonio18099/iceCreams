a6f24320c0e1bb3041449aaa0dd9164223c9c520
antonio18099 <antoniosanchezr0921@gmial.com>
2024-08-10 09:04:05 -0500

    primer commit

---
+++ scripts/app.js
@@ -0,0 +1,48 @@
+import  { iceCreams } from "./products.js"
+
+const container=document.getElementById('main-container')
+
+let car = []
+console.log(iceCreams)
+window.addEventListener('DOMContentLoaded', renderIceCreams)
+
+function renderIceCreams() {
+    iceCreams.forEach(iceCream => createCardIceCream(iceCream));
+}
+
+function createCardIceCream(iceCream) {
+    const divCard=document.createElement('div')
+    divCard.classList.add('ice-cream')
+    divCard.id=iceCream.id
+    
+    const imgCard=document.createElement('img')
+    imgCard.alt=iceCream.product
+    imgCard.src=iceCream.image
+
+    const titleCard=document.createElement('h2')
+    titleCard.textContent=iceCream.product
+
+    const priceCard=document.createElement('p')
+    priceCard.textContent=iceCream.price
+
+    const btnCard=document.createElement('button')
+    btnCard.textContent='Agregar al carrito'
+    btnCard.id = iceCream.id
+
+    btnCard.addEventListener('click',(event) =>{
+        console.log(iceCream.product, iceCream.price)
+    })
+    
+    divCard.appendChild(imgCard)
+    divCard.appendChild(titleCard)
+    divCard.appendChild(priceCard)
+    divCard.appendChild(btnCard)
+    
+    container.appendChild(divCard)
+}
+
+
+
+
+
+{/* <i class='bx bxs-bowl-rice'></i> */}
